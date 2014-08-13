71f1d966df8eacfca01d15c36bfb58448f3dee17
Michael Hilton <michael.hilton@gmail.com>
2014-08-13 16:48:42 -0700

    initial commit of something that is starting
    to look ok, but still has lots of work left to do

---
+++ TransformationClassificationTool.js
@@ -0,0 +1,11 @@
+
+
+function loadTDDCycles(){
+     console.log(JSON);
+     var obj = jQuery.parseJSON( '{ "name": "John" }' );
+     alert( obj.name === "John" );
+    }
+
+
+
+var JSON = '["CurrPhase":{"CycleType":"red","CycleStart":"0","CycleEnd":"315"}}]';
